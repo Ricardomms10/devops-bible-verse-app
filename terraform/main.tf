171a434bcf510faabe18e9d59b5f49190f9559bd
Ricardo Malta <125774953+Ricardomms10@users.noreply.github.com>
2026-03-06 16:11:23 -0300

    terraform restaurado2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "devops_server" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "devops-bible-app"
